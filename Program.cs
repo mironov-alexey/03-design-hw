d168bc1dd72d477398439941dacc5d97ef073d43
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2015-12-05 15:44:33 +0500

    Разбил GetKernel

--- Program.cs
+++ Program.cs
@@ -26,24 +26,38 @@ namespace _03_design_hw
         private static StandardKernel GetKernel(Options options)
         {
             var kernel = new StandardKernel();
+            BindInterfaces(options, kernel);
+            BindSettings(kernel);
+            BindStatistic(kernel);
+            return kernel;
+        }
+
+        private static void BindStatistic(StandardKernel kernel)
+        {
+            var wordsLoader = kernel.Get<WordsLoader>();
+            var stat = kernel.Get<StatisticCalculator>().Calculate(wordsLoader.Words);
+            kernel.Bind<Statistic>().ToConstant(stat).InSingletonScope();
+        }
+
+        private static void BindInterfaces(Options options, StandardKernel kernel)
+        {
             kernel.Bind<Options>().ToConstant(options);
-            kernel.Bind<ISettingsLoader>().To<BaseSettingsLoader>().InSingletonScope();
             kernel.Bind<ICloudGenerator>().To<SimpleCloudGenerator>();
             kernel.Bind<ICloudData>().To<CloudData>();
             kernel.Bind<RectanglePacker>()
                 .ToConstant(new ArevaloRectanglePacker(int.MaxValue, int.MaxValue))
                 .InSingletonScope();
+
+            kernel.Bind<ISettingsLoader>().To<BaseSettingsLoader>().InSingletonScope();
             kernel.Bind<IWordsLoader>().To<WordsLoader>().InSingletonScope();
             kernel.Bind<IBlackListLoader>().To<BlackListLoader>().InSingletonScope();
+            kernel.Bind<IFontCreator>().To<FontCreator>();
+        }
 
+        private static void BindSettings(StandardKernel kernel)
+        {
             var settingsLoader = kernel.Get<ISettingsLoader>();
             kernel.Bind<Settings>().ToConstant(settingsLoader.Load());
-
-            var wordsLoader = kernel.Get<WordsLoader>();
-            var stat = kernel.Get<StatisticCalculator>().Calculate(wordsLoader.Words);
-            kernel.Bind<Statistic>().ToConstant(stat).InSingletonScope();
-            kernel.Bind<IFontCreator>().To<FontCreator>();
-            return kernel;
         }
     }
 }
